aa4454d4884414a3052cee3636c793926145968c
tony <tony@ragku.com>
2020-04-27 21:01:38 +0800

    add ignore

--- src/main/java/com/ragku/leetcode/TwoSum.java
+++ src/main/java/com/ragku/leetcode/TwoSum.java
@@ -15,4 +15,70 @@ public class TwoSum {
         }
         return new int[0];
     }
+
+    public static TwoSum.ListNode getListNode(int... vars) {
+        TwoSum.ListNode node = new TwoSum.ListNode(vars[0]);
+        TwoSum.ListNode tmp = node;
+        for (int i = 1; i < vars.length; i++) {
+            TwoSum.ListNode newNode = new TwoSum.ListNode(vars[i]);
+            tmp.setNext(newNode);
+            tmp = tmp.getNext();
+        }
+        return node;
+    }
+
+    public ListNode twoSum(ListNode node1, ListNode node2) {
+        ListNode result = null, tmp = null;
+        int carry = 0;
+        do {
+            if (null == node1 && null == node2) {
+                break;
+            }
+            int add = carry;
+            if (null != node1) {
+                add += node1.val;
+                node1 = node1.next;
+            }
+
+            if (null != node2) {
+                add += node2.val;
+                node2 = node2.next;
+            }
+
+            if (null == tmp) {
+                result = tmp = new ListNode(add % 10);
+            } else {
+                tmp.next = new ListNode(add % 10);
+                tmp = tmp.next;
+            }
+            carry = add / 10;
+
+        } while (true);
+        if (carry > 0) {
+            tmp.next = new ListNode(carry);
+            tmp = tmp.next;
+        }
+        return result;
+    }
+
+    public static class ListNode {
+        private int val;
+        private ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+
+        public ListNode getNext() {
+            return this.next;
+        }
+
+        public void setNext(ListNode node) {
+            this.next = node;
+        }
+
+        public int getVal() {
+            return val;
+        }
+    }
 }
--- src/test/java/com/ragku/leetcode/TwoSumTest.java
+++ src/test/java/com/ragku/leetcode/TwoSumTest.java
@@ -2,7 +2,7 @@ package com.ragku.leetcode;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertArrayEquals;
 
 public class TwoSumTest {
 
@@ -12,5 +12,21 @@ public class TwoSumTest {
         int[] expect = new int[]{0, 1};
         int[] actual = twoSum.twoSum(new int[]{1, 3, 5, 7}, 4);
         assertArrayEquals(expect, actual);
+    }
+
+    @Test
+    public void twoSum2() {
+        TwoSum twoSum = new TwoSum();
+        TwoSum.ListNode node1 = TwoSum.getListNode(5);
+        TwoSum.ListNode node2 = TwoSum.getListNode(5);
+        TwoSum.ListNode result = twoSum.twoSum(node1, node2);
+        int step = 1;
+        int value = 0;
+        while (null != result) {
+            value += result.getVal() * step;
+            step *= 10;
+            result = result.getNext();
+        }
+        System.out.println(value);
     }
 }
